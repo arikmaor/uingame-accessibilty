4901c0dd580ef2e4c83cf86227eb3fee4b3db2fc
Arik Maor <arikmaor@gmail.com>
2018-10-06 13:20:05 +0300

    Update text

--- src/Settings.js
+++ src/Settings.js
@@ -63,7 +63,7 @@ export default class SettingsModal extends React.Component {
               onValueChange={val => this.setSetting('isDeaf', val)}
             >
               <Picker.Item label="לקות ראייה" value={false} />
-              <Picker.Item label="כבד שמיעה" value={true} />
+              <Picker.Item label="לקות שמיעה" value={true} />
             </Picker>
           </View>
           { this.state.settings.isDeaf && (
